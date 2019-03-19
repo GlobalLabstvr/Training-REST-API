5fb746c6f814418858cadf4f90d88850500dbf31
Kathambaritvr <sujeesatheeshveera@gmail.com>
2019-03-19 13:53:48 +0530

    playlist working

--- src/main/java/com/tvr/training/api/playlist/PlaylistController.java
+++ src/main/java/com/tvr/training/api/playlist/PlaylistController.java
@@ -1,5 +1,6 @@
 package com.tvr.training.api.playlist;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,24 +10,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tvr.training.api.exception.ResourceNotFoundException;
+import com.tvr.training.api.topic.Topic;
 import com.tvr.training.api.topic.TopicRepository;
 
 @RestController
 public class PlaylistController {
 
     @Autowired
-    private PlaylistRepository playlistRepository;
+    private TopicRepository topicRepository;
 
     @Autowired
-    private TopicRepository topicRepository;
+    private PlaylistRepository playlistRepository;
     
     @GetMapping("/playlists")
-    public List<Playlist> getAllPlaylists( ) {
-        return playlistRepository.findAll( );
+    public List<Playlist> getAllPlaylists(
+    		@RequestParam("name") String name) {
+        List<Playlist> list = new ArrayList<Playlist>();
+    	if(name!=null && !name.equals("")) {
+        	list = playlistRepository.findByNameContaining(name);
+        }
+    	else {
+    		list = playlistRepository.findAll();
+    	}
+    	return list;
     }
     
     @GetMapping("playlists/{playlistId}")
@@ -35,14 +47,8 @@ public class PlaylistController {
         return playlistRepository.findById(playlistId);
     }
     
-    @GetMapping("/topics/{topicId}/playlists")
-    public List<Playlist> getPlaylistsByTopicId(
-    		@PathVariable (value = "topicId") Long topicId) {
-        return playlistRepository.findByTopicId(topicId);
-    }
-    
        
-    @PostMapping("/topics/{topicId}/playlists")
+    @PostMapping("/playlists/{topicId}")
     public Playlist createTopic(
     		@PathVariable (value = "topicId") Long topicId,
     		@Valid @RequestBody Playlist playlist) {
@@ -50,6 +56,18 @@ public class PlaylistController {
             playlist.setTopic(topic);         
             return playlistRepository.save(playlist);
         }).orElseThrow(() -> new ResourceNotFoundException("topicId " + topicId + " not found"));
+    }
+
+    @PutMapping("/playlists")
+    public Playlist updatePlaylist(@Valid @RequestBody Playlist playlistRequest) {
+    	Long playlistId = playlistRequest.getId();
+        return playlistRepository.findById(playlistId).map(playlist -> {
+        	playlist.setTopic(playlistRequest.getTopic());
+            playlist.setName(playlistRequest.getName());
+            playlist.setDescription(playlistRequest.getDescription());
+            playlist.setUrl(playlistRequest.getUrl());
+          return playlistRepository.save(playlist);
+        }).orElseThrow(() -> new ResourceNotFoundException("playlistId " + playlistId + "not found"));
     }
 
 }
--- src/main/java/com/tvr/training/api/playlist/PlaylistRepository.java
+++ src/main/java/com/tvr/training/api/playlist/PlaylistRepository.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.tvr.training.api.topic.Topic;
+
 @Repository
 
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
@@ -13,5 +15,7 @@ public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
 	Optional<Playlist> findById(Long playlistId);
 
 	List<Playlist> findByTopicId(Long topicId);
+	
+	List<Playlist> findByNameContaining(String name);
 	
 }
