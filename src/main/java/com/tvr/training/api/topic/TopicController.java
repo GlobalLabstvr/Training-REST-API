f12180bf5593393622aec732c3ec84274c0f5a04
Suresh Selvaraj <suresh.selvaraj5@cognizant.com>
2019-03-24 22:46:21 +0530

    TopicVO implemented

--- src/main/java/com/tvr/training/api/topic/TopicController.java
+++ src/main/java/com/tvr/training/api/topic/TopicController.java
@@ -16,7 +16,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tvr.training.api.exception.ResourceNotFoundException;
-import com.tvr.training.api.subject.Subject;
+import com.tvr.training.api.model.PlaylistVO;
+import com.tvr.training.api.model.TopicVO;
+import com.tvr.training.api.playlist.Playlist;
+import com.tvr.training.api.playlist.PlaylistRepository;
 import com.tvr.training.api.subject.SubjectRepository;
 
 @RestController
@@ -27,6 +30,9 @@ public class TopicController {
 
     @Autowired
     private SubjectRepository subjectRepository;
+    
+    @Autowired
+    private PlaylistRepository playlistRepository;
     
     @GetMapping("/topics")
     public List<Topic> getAllsubjects(
@@ -41,15 +47,28 @@ public class TopicController {
     	return list;
     }
     
-    @GetMapping("topics/{topicId}")
-    public Optional<Topic> getTopic(
+    @GetMapping("/topics/{topicId}")
+    public Optional<Topic> getTopicById(
     		@PathVariable (value = "topicId") Long topicId) {
-        return topicRepository.findById(topicId);
+       return topicRepository.findById(topicId);
+    }
+    
+    @GetMapping("/topics/subjects/{subjectId}")
+    public TopicVO getTopicBySubjectId(
+    		@PathVariable (value = "subjectId") Long subjectId) {
+        List<Topic> topic = topicRepository.findBySubjectId(subjectId);
+        TopicVO topicVO = copy(topic.get(0));
+    	List<Playlist>playlists = playlistRepository.findByTopicId(topicVO.getId());
+    		playlists.forEach(playlist -> {
+    			PlaylistVO playlistVO = copy(playlist);
+    			topicVO.getPlaylists().add(playlistVO);
+    		});
+    	return topicVO;
     }
     
        
     @PostMapping("/topics/{subjectId}")
-    public Topic createSubject(
+    public Topic createTopic(
     		@PathVariable (value = "subjectId") Long subjectId,
     		@Valid @RequestBody Topic topic) {
         return subjectRepository.findById(subjectId).map(subject -> {
@@ -67,6 +86,23 @@ public class TopicController {
             topic.setDescription(topicRequest.getDescription());
             return topicRepository.save(topic);
         }).orElseThrow(() -> new ResourceNotFoundException("TopicId " + topicId + "not found"));
+    }
+    
+    private TopicVO copy(Topic topic) {
+    	TopicVO topicVO = new TopicVO();
+    	topicVO.setId(topic.getId());
+    	topicVO.setName(topic.getName());
+    	topicVO.setDescription(topic.getDescription());
+    	return topicVO;
+    }
+    
+    private PlaylistVO copy(Playlist playlist) {
+    	PlaylistVO playlistVO = new PlaylistVO();
+    	playlistVO.setId(playlist.getId());
+    	playlistVO.setName(playlist.getName());
+    	playlistVO.setDescription(playlist.getDescription());
+    	playlistVO.setUrl(playlist.getUrl());
+    	return playlistVO;
     }
 
 }
